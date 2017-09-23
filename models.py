1bf3ab7d8938be4d7abcca232c3cc23f0c6fcb00
srikanthmalla <mallasrikanth004@gmail.com>
2017-09-23 18:32:30 -0400

    intendation corrected on cluster

--- models.py
+++ models.py
@@ -40,24 +40,24 @@ class a2c:
 		self.writer = tf.summary.FileWriter(tf_logdir, self.sess.graph)
 		self.log_reward=tf.summary.scalar("totalreward", tf.reduce_sum(self.total_reward))
 		self.log_policyloss=tf.summary.scalar("actor_loss",tf.reduce_mean(self.loss_policy))
-        self.log_criticloss=tf.summary.scalar("critic_loss",tf.reduce_mean(self.loss_value))
-        #self.summary=tf.summary.merge_all()
+		self.log_criticloss=tf.summary.scalar("critic_loss",tf.reduce_mean(self.loss_value))
+		#self.summary=tf.summary.merge_all()
 
 		self.init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 		self.sess.run(self.init_op)
 		self.default_graph = tf.get_default_graph()
 		self.default_graph.finalize() # avoid modifications
 	
-        def save(self,step):
+	def save(self,step):
 		self.saver.save(self.sess, './tmp/model.ckpt', global_step=step)
 
 	def predict_value(self,observation):
 		v=self.sess.run(self.v,feed_dict={self.observation:observation})
 		return v
 
-    def predict_action_prob(self,observation):
-        a=self.sess.run(self.p,feed_dict={self.observation:observation})
-        return a
+	def predict_action_prob(self,observation):
+		a=self.sess.run(self.p,feed_dict={self.observation:observation})
+		return a
 
 	def actor(self,inputs): #modified vgg net
 		actions=FCN_one_hidden(inputs,10,no_of_actions,'actor/')
@@ -67,15 +67,15 @@ class a2c:
 		value=FCN_one_hidden(inputs,10,1,'value/')
 		return value
         
-    def train_actor(self, observations, actions, R, step):
-        [_, policyloss]=self.sess.run([self.actor_optimizer, self.log_policyloss], feed_dict={self.observation:observations, self.a_t:actions, self.R:R})
-        self.writer.add_summary(policyloss, step)
+	def train_actor(self, observations, actions, R, step):
+		[_, policyloss]=self.sess.run([self.actor_optimizer, self.log_policyloss], feed_dict={self.observation:observations, self.a_t:actions, self.R:R})
+		self.writer.add_summary(policyloss, step)
 
-    def train_critic(self, observations, R, step):      
-        [_, criticloss]=self.sess.run([self.critic_optimizer, self.log_criticloss], feed_dict={self.observation:observations, self.R:R})
-        self.writer.add_summary(criticloss, step)
+	def train_critic(self, observations, R, step):      
+		[_, criticloss]=self.sess.run([self.critic_optimizer, self.log_criticloss], feed_dict={self.observation:observations, self.R:R})
+		self.writer.add_summary(criticloss, step)
 
-    #useful to log other details like features, rewards
-    def log_details(self, total_reward, step):
-        tot_r=self.sess.run(self.log_reward, feed_dict={self.total_reward:total_reward})   
-        self.writer.add_summary(tot_r, step) 
+	#useful to log other details like features, rewards
+	def log_details(self, total_reward, step):
+		tot_r=self.sess.run(self.log_reward, feed_dict={self.total_reward:total_reward})   
+		self.writer.add_summary(tot_r, step) 
